4129615c7565f242bbacad0b3b873687972948de
SOL1archive <enlightkorean@gmail.com>
2023-01-18 20:03:43 +0900

    add:prepare_dataset

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 import datetime
+import logging
+
+import pandas as pd
 
 import torch
 import torch.nn as nn
@@ -8,59 +11,104 @@ from torch.utils.tensorboard import SummaryWriter
 from Data.Dataset import FinTextDataset
 from Data.DataLoader import FinTextDataLoader
 from Model.MainModel import FinTextModel
+from utils import concat_dataset
+
+log = logging.getLogger(__name__)
+
+# log.setLevel(logging.WARN)
+log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
+
+METRICS_LABEL_NDX = 0
+METRICS_PRED_NDX = 1
+METRICS_LOSS_NDX = 2
+METRICS_SIZE = 3
+
 
 class TrainingApp:
-    def __init__(self):
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.writer = SummaryWriter(log_dir='./runs')
-        self.datetime = datetime.datetime.now()
+    def __init__(self, **config):
+        default_config = {
+            "num_epoch": 30,
+            "lr": 0.001,
+            "df_list": [
+                pd.read_csv("./data-dir/kakao.xlsx"),
+                pd.read_csv("./data-dir/spc.xlsx"),
+            ],
+        }
+
+        for key in default_config.keys():
+            if key not in config.keys():
+                config[key] = default_config[key]
+
+        self.config = config
+        self.df_list = self.config["df_list"]
+        self.num_epoch = self.config["num_epoch"]
+
+        self.use_cuda = torch.cuda.is_available()
+        self.device = torch.device("cuda" if self.use_cuda else "cpu")
+
+        self.trn_writer = None
+        self.val_writer = None
+        self.totalTrainingSamples_count = 0
+
+        self.time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
 
     def prepare_dataset(self):
-        self.dataset = FinTextDataset().to(self.device)
+        self.dataset = concat_dataset(self.df_list)
+        self.dataset.to(self.device)
+
         self.dataloader = FinTextDataLoader(self.dataset)
 
     def prepare_model(self):
-        self.model = FinTextModel().to(self.device)
+        self.model = FinTextModel()
+        if self.use_cuda:
+            log.info("Using CUDA; {} devices.".format(torch.cuda.device_count()))
+            if torch.cuda.device_count() > 1:
+                self.model = nn.DataParallel(self.model)
+            self.model = self.model.to(self.device)
+
+    def prepare_optimizer(self):
+        self.optimizer = Adam(self.model.parameters(), lr=self.config["lr"])
 
     def train(self):
         criterion = nn.CrossEntropyLoss().to(self.device)
-        optimizer = Adam(self.model.parameters())
 
-        for epoch in range(self.dataloader):
+        for epoch in range(self.num_epoch):
             for i, (input_tensor, labels) in enumerate(self.dataloader):
                 # Forward
                 input_tensor = input_tensor.to(self.device)
                 output_tensor = self.model(input_tensor)
-                
+
                 loss = criterion(output_tensor, labels)
                 self.writer.add_scalar()
 
                 # Backward
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
                 loss.backward()
-                optimizer.step()
+                self.optimizer.step()
 
-            print(f'EPOCH: {epoch}, Training Loss {loss.item():.4f}')
+            print(f"EPOCH: {epoch}, Training Loss {loss.item():.4f}")
 
         self.writer.flush()
 
     def main(self):
         self.prepare_dataset()
         self.prepare_model()
+        self.prepare_optimizer()
         self.train()
 
-        save_model = input('Save Model(Y/N)? ')
-        if save_model.upper() == 'Y':
+        save_model = input("Save Model(Y/N)? ")
+        if save_model.upper() == "Y":
             torch.save(
-                self.model, 
-                f"./model-dir/{self.datetime.strftime('%d_%b_%Y_%H:%M:%S')}.model"
+                self.model,
+                f"./model-dir/{self.datetime.strftime('%d_%b_%Y_%H:%M:%S')}.model",
             )
 
         try:
-            print('Press Ctrl + C to turn off the TensorBoard')
+            print("Press Ctrl + C to turn off the TensorBoard")
             _ = input()
         except KeyboardInterrupt:
             self.writer.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     TrainingApp().main()
--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import torch
+import pandas as pd
+
+from Data.Dataset import FinTextDataset    
+
 def divide_chunks(lt, n):
      
     for i in range(0, len(lt), n):
@@ -6,3 +11,18 @@ def divide_chunks(lt, n):
 def to(tensor, device):
     tensor.to(device)
     
+def concat_dataset(df_list):
+    dataset_lt = []
+    for df in df_list:
+        dataset_lt.append(FinTextDataset(df))
+    
+    dataset = dataset_lt[0]
+    for dataset_item in dataset_lt[1:]:
+        dataset.feature_df = pd.concat(
+            [dataset.feature_df, dataset_item.feature_df]
+        )
+        dataset.target_tensor = torch.concat(
+            [dataset.target_tensor, dataset_item.target_tensor]
+        )
+    
+    return dataset
