3c098a22c09aaa769b9be84ae7cd278951512b68
BOBkumo <leeseungyun123@gmail.com>
2023-02-04 18:43:42 +0900

    add super parameter self

--- Model/SubModel.py
+++ Model/SubModel.py
@@ -14,7 +14,7 @@ class GRU(nn.Module):
 
 class StdCNN2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, use_batch_norm=True):
-        super(StdCNN2d).__init__()
+        super(self, StdCNN2d).__init__()
 
         self.use_batch_norm = use_batch_norm
 
